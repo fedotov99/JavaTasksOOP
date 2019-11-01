caa7e7d4cb8049d1f0499c9a703af58f85beba73
Vlad Fedotov <fedotovvlad@list.ru>
2019-11-01 21:05:00 +0300

    MyPoint added

---
+++ src/com/company/MyPoint.java
@@ -0,0 +1,57 @@
+package com.company;
+
+public class MyPoint {
+    int x = 0;
+    int y = 0;
+
+    public MyPoint() {
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int[] getXY() {
+        int[] xy = {getX(), getY()};
+        return xy;
+    }
+
+    public void setXY(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + getX() + ", " + getY() + ")";
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+    }
+
+    public double distance(MyPoint point) {
+        return Math.sqrt((point.x - this.x) * (point.x - this.x) + (point.y - this.y) * (point.y - this.y));
+    }
+
+    public double distance() {
+        return Math.sqrt((this.x) * (this.x) + (this.y) * (this.y));
+    }
+}
